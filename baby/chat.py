f940e28f542ea26a597ef017970def894c81a2f2
durban.zhang <durban.zhang@gmail.com>
2019-10-15 16:52:58 +0800

    update chat

--- baby/chat.py
+++ baby/chat.py
@@ -4,10 +4,21 @@
 
 import random
 
-from flask import Blueprint, render_template, request, current_app, g
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    current_app,
+    g,
+    session
+)
 from baby.extensions import socketio
 from flask_socketio import emit, join_room, leave_room, close_room, rooms
 
+online_nicks = []
+online_sid = []
+online_sid_dict = {}
+online_num = 0
 
 bp = Blueprint('chat', __name__, url_prefix='/chat')
 
@@ -31,17 +42,22 @@ def index():
 
 @socketio.on('connect', namespace='/chat')
 def handle_connect():
-    emit('response', {'data': 'Connected', 'type': 'tips'})
+    emit('response', {
+        'data': 'Connected',
+        'type': 'tips'
+    })
 
 
 @socketio.on('message', namespace='/chat')
 def handle_message(message):
     # 信息发到服务器进行过滤
+
     emit('response', message)
 
 
 @socketio.on('broadcast_event', namespace='/chat')
 def handle_broadcast_event(msg):
+    global online_num
     nick = None
     avatar_bg_color = 'grey'
 
@@ -52,12 +68,35 @@ def handle_broadcast_event(msg):
         avatar_bg_color = msg['avatar_bg_color']
 
     if nick is not None:
+        if nick in online_nicks and request.sid not in online_sid:
+            emit('error', {
+                'data': '昵称已经存在',
+                'type': 'alert'
+            })
+            return True
+        elif request.sid in online_sid_dict and online_sid_dict[request.sid] != nick:
+            emit('error', {
+                'data': '不能更换昵称',
+                'type': 'alert'
+            })
+            return True
+        else:
+            if request.sid not in online_sid:
+                online_num += 1
+                session['nick'] = nick
+
+                online_nicks.append(nick)
+                online_sid.append(request.sid)
+
+                online_sid_dict[request.sid] = nick
+
         nick = nick[:1]
         emit('response', {
             'data': msg['data'],
             'type': msg['type'],
             'nick': nick,
-            'avatar_bg_color': avatar_bg_color
+            'avatar_bg_color': avatar_bg_color,
+            'online_num': online_num
         }, broadcast=True)
 
 
@@ -73,20 +112,39 @@ def ping_pong():
 
 @socketio.on('join', namespace='/chat')
 def hanle_join(message):
-    emit('response', {'data': 'In Rooms', 'type': 'tips'})
+    emit('response', {
+        'data': 'In Rooms',
+        'type': 'tips',
+    })
 
 
 @socketio.on('leave', namespace='/chat')
 def handle_leave(message):
-    emit('response', {'data': 'Out Rooms', 'type': 'tips'})
+    emit('response', {
+        'data': 'Out Rooms',
+        'type': 'tips',
+    })
 
 
 @socketio.on('disconnect', namespace='/chat')
 def handle_disconnect():
+    global online_num
+    nick = session.get('nick')
+    if request.sid in online_sid_dict:
+        online_num -= 1
+        if online_num <= 0:
+            online_num = 0
+
+        del online_sid_dict[request.sid]
+        online_nicks.remove(session['nick'])
+
     print('Client Disconnect', request.sid)
 
-    emit('response', {'data': 'Client Disconnect',
-                      'type': 'tips'}, broadcast=True)
+    emit('response', {
+        'data': session['nick'] + 'Client Disconnect',
+        'type': 'tips',
+        'online_num': online_num
+    }, broadcast=True)
 
 
 @socketio.on_error('/chat')  # handles the '/chat' namespace

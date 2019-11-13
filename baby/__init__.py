af1fb5d926c2cd5370586fbdb46bdebb0cc8d88d
durban.zhang <durban.zhang@gmail.com>
2019-11-13 15:07:38 +0800

    optimize code

--- baby/__init__.py
+++ baby/__init__.py
@@ -1,19 +1,14 @@
 #! _*_ coding: utf-8 _*_
 
 import os
-import logging
 
 from flask import (
     Flask,
-    has_request_context,
-    request,
     jsonify
 )
 
-from raven.contrib.flask import Sentry
 from logging.config import dictConfig
-from logging.handlers import SMTPHandler
-from baby.extensions import socketio, cache, mongodb
+from baby.extensions import socketio
 from baby import command
 from baby import views
 from baby import db
@@ -22,79 +17,12 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.exceptions import BadRequest
 from baby.exception import InvalidUsage
 from baby.middleware import HTTPMethodOverrideMiddleware
-
-
-def logging_common_formatter():
-    class RequestFormatter(logging.Formatter):
-
-        def format(self, record):
-            if has_request_context():
-                record.url = request.url
-                record.remote_addr = request.remote_addr
-            else:
-                record.url = None
-                record.remote_addr = None
-
-            return super().format(record)
-
-    formatter = RequestFormatter(
-        '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
-        '%(levelname)s in %(module)s: %(message)s'
-    )
-
-    return formatter
-
-# 添加mail Handler
-
-
-def register_mail(app):
-    # SMTP Handler
-    if 'MAIL_HOST' in app.config and app.config['MAIL_HOST']\
-            and 'MAIL_PORT' in app.config and app.config['MAIL_PORT']\
-            and 'MAIL_FROM' in app.config and app.config['MAIL_FROM']\
-            and 'MAIL_TO' in app.config and app.config['MAIL_TO']\
-            and 'MAIL_USERNAME' in app.config and app.config['MAIL_USERNAME']\
-            and 'MAIL_PASSWORD' in app.config and app.config['MAIL_PASSWORD']:
-
-        mail_handler = SMTPHandler(
-            (
-                app.config['MAIL_HOST'],
-                app.config['MAIL_PORT'],
-            ),
-            app.config['MAIL_FROM'],
-            app.config['MAIL_TO'],
-            'Application Error',
-            (
-                app.config['MAIL_USERNAME'],
-                app.config['MAIL_PASSWORD'],
-            )
-        )
-
-        mail_handler.setLevel(logging.ERROR)
-        formatter = logging_common_formatter()
-
-        mail_handler.setFormatter(formatter)
-
-        app.logger.addHandler(mail_handler)
-
-        logging.info('register mail handler success')
-
-
-def register_cache(app):
-    if 'CACHE_TYPE' in app.config and app.config['CACHE_TYPE']:
-        cache.init_app(app, config={
-            'CACHE_TYPE': app.config['CACHE_TYPE']
-        })
-
-
-def register_mongodb(app):
-    mongodb.init_app(app)
-
-
-def register_sentry(app):
-    if 'SENTRY_DSN' in app.config and app.config['SENTRY_DSN']:
-        sentry = Sentry(dsn=app.config['SENTRY_DSN'])
-        sentry.init_app(app)
+from baby.helper import (
+    register_cache,
+    register_mail,
+    register_mongodb,
+    register_sentry
+)
 
 
 def create_app(test_config=None):

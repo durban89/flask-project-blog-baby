79526b291a48ad32c03d318d03a3325058aadb14
durban.zhang <durban.zhang@gmail.com>
2019-11-26 17:17:28 +0800

    update version

--- baby/__init__.py
+++ baby/__init__.py
@@ -29,7 +29,6 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(
         __name__,
-        static_folder='static',
         instance_relative_config=True
     )
 

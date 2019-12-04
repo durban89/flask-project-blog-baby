ddf4f21d9b40f6e3ac1f8391ebdbc281eb612bdb
durban.zhang <durban.zhang@gmail.com>
2019-12-04 19:11:35 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 # @Author: durban.zhang
 # @Date:   2019-10-21 13:35:26
 # @Last Modified by:   durban.zhang
-# @Last Modified time: 2019-11-28 10:41:51
+# @Last Modified time: 2019-12-04 19:10:47
 
 from setuptools import find_packages, setup
 
 setup(
     name='baby',
-    version='1.0.12',
+    version='1.0.13',
     author='张大鹏',
     author_email='durban.zhang@gmail.com',
     packages=find_packages(),
